7e5cb01f40dec0e7e92683b12193dc5f17871131
Nanikaamaimono <155080374+Nanikaamaimono@users.noreply.github.com>
2023-12-28 14:40:22 +0100

    Update encrypt.sh

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -3,9 +3,9 @@
 echo "Welcome, I am ready to encrypt a file/folder for you"
 echo "currently I have a limitation, Place me to the same folder, where a file to be encrypted is present"
 echo "Enter the Exact File Name with extension"
-read -r file
+read -r filename
 # decryption command
-# gpg -d filename.gpg > filename
+# gpg -d $filename.gpg > $filename
 gpg -c "$file"
 echo "I have encrypted the file sucessfully..."
 echo "Now I will be removing the original file"
